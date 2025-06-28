266b84bd8be068f046e129564396332c64277196
fgclue <lost@biitle.nl>
2025-06-28 00:21:00 -0300

    Add temporariy isRunning function

--- gasoline/emulator.py
+++ gasoline/emulator.py
@@ -2,8 +2,20 @@ from subprocess import check_output
 from gasoline import adb
 from os import PathLike
 
-def isRunning(name: str, adb: PathLike[str]) -> bool:
+def isRunning(name: str, adb: PathLike[str]) -> bool | str:
+    #TODO: REPLACE WITH ADB CONNECTION
+    """
+    Returns False if there is no device running or an str with the name of the device.
+    """
     result = check_output('%s devices' % adb, shell=True)
 
-    print(result)
+    lines = result.decode("utf-8").split('\n')[1:]
+
+    for line in lines:
+        deviceName = line.split('\t')[0]
+        if deviceName == '': continue
+
+        adbName = check_output('%s -s %s emu avd name' % (adb, deviceName), shell=True).decode('utf-8').split('\r')[0]
+        if adbName == name.removeprefix("@"): return deviceName
+    
     return False
