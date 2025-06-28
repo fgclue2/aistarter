ad16eba6257632db00ec55913f6d83f5eeab8ae4
fgclue <lost@biitle.nl>
2025-06-27 21:07:40 -0300

    Create flask server interface

---
+++ gasoline/__init__.py
@@ -0,0 +1,57 @@
+from os import PathLike
+from pathlib import Path
+from flask import Flask, request
+from gasoline.emulator import isRunning
+
+# TODO: replace rall calls to adb with the adb protocol
+
+app = Flask(__name__)
+
+adb: PathLike[str] | None = None
+emulator: PathLike[str] | None = None
+
+emulatorName: str | None = None
+
+# this makes no fucking sense but it works: https://github.com/mit-cml/appinventor-sources/blob/master/appinventor/misc/emulator-support/config.py
+VERSION = '%d.%d.%d%s' % (26, 255, 0, '')
+
+@app.after_request
+def add_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Headers'] = 'origin, content-type'
+    response.headers['Content-Type'] = 'application/json'
+
+    return response
+
+@app.route('/ping/')
+def ping():
+    return {
+        "status": "OK",
+        "version": VERSION
+    }
+
+@app.route('/utest/')
+def utest():
+    device = isRunning(emulatorName, adb)
+    if device:
+        return {
+            "status": "OK",
+            "device": device,
+            "version": VERSION
+        }
+    else:
+        return {
+            "status": "NO",
+            "version": VERSION
+        }
+
+def start(adb_: PathLike[str], emulator_: PathLike[str], name: str):
+    global adb
+    global emulator
+    global emulatorName
+    
+    adb = Path(adb_)
+    emulator = Path(emulator_)
+    emulatorName = name
+
+    app.run(port=8004)
