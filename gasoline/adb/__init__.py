911d0fb791df2d09a92a317dd6370a86ba548822
fgclue <lost@biitle.nl>
2025-06-29 00:02:47 -0300

    Clean up adb

--- gasoline/adb/__init__.py
+++ gasoline/adb/__init__.py
@@ -1,26 +1,14 @@
 from enum import Enum
-from ctypes import c_uint32
+from socket import create_connection
 
-MAX_ADB_DATA = 4096
-PROTOCOL_VERSION = 0x01000000
-
-AUTH_TOKEN = 1
-AUTH_SIGNATURE = 2
-AUTH_RSAPUBLICKEY = 3
-
-A_SYNC = c_uint32(0x434E5953)
-A_CNXN = c_uint32(0x4E584E43)
-A_OPEN = c_uint32(0x4E45504F)
-A_OKAY = c_uint32(0x59414B4F)
-A_CLSE = c_uint32(0x45534C43)
-A_WRTE = c_uint32(0x45545257)
-
+class AdbError(Exception):
+    def __init__(self, *args):
+        super().__init__(*args)
 
 class SystemType(Enum):
     Bootloader = "bootloader"
     Device = "device"
     Host = "host"
-
 
 class SystemIdentityString:
     def __init__(self, systemType: SystemType, serialNumber: str, banner: str):
@@ -30,3 +18,10 @@ class SystemIdentityString:
 
     def __str__(self):
         return f"{self.systemType}:{self.serialNumber}:{self.banner}"
+
+class AdbClient():
+    def __init__(self, host: str, port: int):
+        self.connection = create_connection((host, port))
+
+    def send_command(self, command: str):
+        self.connection.send(f"{len(command):04x}".encode() + command.encode())
